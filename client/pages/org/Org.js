a6d68bf249e72007641518d0456f9a7bd09c7cc5
黄嘉庆 <657593825@qq.com>
2018-04-28 17:27:19 +0800

    feat: add org's page

---
+++ client/pages/org/Org.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Org extends Component {
+
+  render() {
+
+    return (
+      <div>
+        组织页
+      </div>
+    );
+  }
+}
--- client/routes.js
+++ client/routes.js
@@ -2,11 +2,17 @@ import Loadable from './utils/loadable';
 
 const routes = [
   {
-    name: '登录页',
+    name: '登录',
     path: '/login',
     exact: true,
     component: Loadable(() => import('./pages/login'))
-  }
+  },
+  {
+    name: '组织',
+    path: '/org',
+    exact: true,
+    component: Loadable(() => import('./pages/org'))
+  },
 ];
 
 export default routes;
