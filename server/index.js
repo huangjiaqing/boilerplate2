08143f8919699a28abcf9bf1294eb769abdd38d2
黄嘉庆 <657593825@qq.com>
2018-04-28 16:34:28 +0800

    style: server index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,5 @@
 import R from 'ramda';
 import Koa from 'koa';
-// import Router from 'koa-router';
-// import logger from 'koa-logger';
-// import views from 'koa-views';
-// import serve from 'koa-static';
 import { resolve } from 'path';
 
 const MIDDLEWARES = ['common', 'router'];
@@ -27,6 +23,7 @@ const useMiddleware = (app) => {
   useMiddleware(app);
 
   if (R.equals(process.env.NODE_ENV, 'development')) {
+    // 开发模式下启动前端dev
     require('../tools/dev');
   }
 
@@ -35,31 +32,3 @@ const useMiddleware = (app) => {
   });
 
 })(); 
-
-// const r = (path='') => resolve(__dirname, `../${path}`);
-
-// const app = new Koa();
-// const router = new Router();
-
-// router.get('/api', async (ctx, next) => {
-//   ctx.body = {
-//     msg: '你好呀'
-//   };
-// });
-
-// app.use(logger());
-
-// app.use(router.routes());
-
-// app.use(serve(r('dist')));
-
-// app.use(views(r('dist'), {
-//   extension: 'html'
-// }));
-
-// app.use(async (ctx, next) => {
-//   await ctx.render('index.html')
-//   await next();
-// });
-
-// module.exports = app;
